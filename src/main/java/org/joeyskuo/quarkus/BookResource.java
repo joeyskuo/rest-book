53d179f05d5938035f2abb567252d343ab716140
Joey Kuo <joeyskuo@gmail.com>
2024-07-28 18:55:16 -0700

    getAllBooks returns List for GET endpoint

--- src/main/java/org/joeyskuo/quarkus/BookResource.java
+++ src/main/java/org/joeyskuo/quarkus/BookResource.java
@@ -5,14 +5,19 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+import java.util.List;
+
 @Path("/api/books")
 public class BookResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        System.out.println("hello");
-        return "Hello World";
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Book> getAllBooks() {
+        return List.of(
+            new Book(1, "Understanding Quarkus", "jkuo", 2024, "Java"),
+            new Book(2, "Effective Java", "jkuo", 2022, "Java"),
+            new Book(3, "Practicing Quarkus", "antonio", 2020, "IT")
+        );
     }
 
     @GET
