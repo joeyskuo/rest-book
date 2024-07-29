a70b2f167c675b819c64af2e4cc8588e419e4640
Joey Kuo <joeyskuo@gmail.com>
2024-07-28 20:55:16 -0700

    Inject logger and log endpoints

--- src/main/java/org/joeyskuo/quarkus/BookResource.java
+++ src/main/java/org/joeyskuo/quarkus/BookResource.java
@@ -5,6 +5,7 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.jboss.logging.Logger;
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
 import java.util.List;
@@ -16,9 +17,13 @@ public class BookResource {
     @Inject
     BookRepository bookRepository;
 
+    @Inject
+    Logger logger;
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Book> getAllBooks() {
+        logger.info("/api/books");
         return bookRepository.getAllBooks();
     }
 
@@ -26,12 +31,14 @@ public class BookResource {
     @Path("/count")
     @Produces(MediaType.TEXT_PLAIN)
     public int countAllBooks() {
+        logger.info("/api/books/count");
         return bookRepository.countAllBooks();
     }
 
     @GET
     @Path("{id}")
     public Optional<Book> getBookById(@PathParam("id") int id) {
+        logger.info("/api/books/" + id);
         return bookRepository.getBookById(id);
     }
 
@@ -39,6 +46,7 @@ public class BookResource {
     @Path("/page")
     @Produces(MediaType.TEXT_HTML)
     public String helloHtml() {
+        logger.info("/api/books/page");
         return "<div>" +
                 "<h1>Book API</h1>" +
                 "<div>Hello World</div>" +
