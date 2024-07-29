eb547fc3b60d6bdad2a51edc9143ef0d57cea0da
Joey Kuo <joeyskuo@gmail.com>
2024-07-28 18:54:54 -0700

    yearOfPublication is an int type

--- src/main/java/org/joeyskuo/quarkus/Book.java
+++ src/main/java/org/joeyskuo/quarkus/Book.java
@@ -5,10 +5,10 @@ public class Book {
     public int id;
     public String title;
     public String author;
-    public String yearOfPublication;
+    public int yearOfPublication;
     public String genre;
 
-    public Book(int id, String title, String author, String yearOfPublication, String genre) {
+    public Book(int id, String title, String author, int yearOfPublication, String genre) {
         this.id = id;
         this.title = title;
         this.author = author;
