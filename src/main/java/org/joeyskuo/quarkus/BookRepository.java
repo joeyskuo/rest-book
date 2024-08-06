7f5b216a3b3dd12bb393b08bc8dc9c21257fc0ff
Joey Kuo <joeyskuo@gmail.com>
2024-08-06 10:02:47 -0700

    use ConfigProperty for genre

--- src/main/java/org/joeyskuo/quarkus/BookRepository.java
+++ src/main/java/org/joeyskuo/quarkus/BookRepository.java
@@ -5,6 +5,7 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
 import java.util.List;
@@ -13,11 +14,14 @@ import java.util.Optional;
 @ApplicationScoped
 public class BookRepository {
 
+    @ConfigProperty(name = "books.genre", defaultValue = "Coding")
+    String genre;
+
     public List<Book> getAllBooks() {
         return List.of(
-            new Book(1, "Understanding Quarkus", "jkuo", 2024, "Java"),
-            new Book(2, "Effective Java", "jkuo", 2022, "Java"),
-            new Book(3, "Practicing Quarkus", "antonio", 2020, "IT")
+            new Book(1, "Understanding Quarkus", "jkuo", 2024, genre),
+            new Book(2, "Effective Java", "jkuo", 2022, genre),
+            new Book(3, "Practicing Quarkus", "antonio", 2020, genre)
         );
     }
 
